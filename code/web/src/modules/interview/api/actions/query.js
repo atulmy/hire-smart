c83a5f7988d5cb84cb6a3983936472854c428048
atulmy <atul.12788@gmail.com>
2018-07-01 23:25:26 +0530

    Localstorage caching [WEB]

--- code/web/src/modules/interview/api/actions/query.js
+++ code/web/src/modules/interview/api/actions/query.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import { API_URL } from '../../../../setup/config/env'
 import { queryBuilder } from '../../../../setup/helpers'
 import { MESSAGE_SHOW } from '../../../common/api/actions'
+import { INTERVIEW_LIST_CACHE, INTERVIEW_SINGLE_CACHE, INTERVIEW_LIST_BY_CLIENT_CACHE } from './cache-keys'
 import {
   LIST_REQUEST,
   LIST_RESPONSE,
@@ -20,16 +21,41 @@ import {
 // Get list
 export function getList(isLoading = true) {
   return async dispatch => {
-    dispatch({
-      type: LIST_REQUEST,
-      isLoading
-    })
+    // Caching
+    try {
+      const list = JSON.parse(window.localStorage.getItem(INTERVIEW_LIST_CACHE))
+
+      if(list) {
+        dispatch({
+          type: LIST_RESPONSE,
+          list
+        })
+      } else {
+        dispatch({
+          type: LIST_REQUEST,
+          isLoading
+        })
+      }
+    } catch(e) {
+      dispatch({
+        type: LIST_REQUEST,
+        isLoading
+      })
+    }
 
     try {
       const { data } = await axios.post(API_URL, queryBuilder({
         type: 'query',
         operation: 'interviewsByOrganization',
-        fields: ['_id', 'organizationId { _id }', 'clientId { _id }', 'candidateId { _id }', 'interviewerId { _id }', 'userId { _id }', 'dateTime', 'mode', 'note', 'createdAt']
+        fields: [
+          '_id',
+          'organizationId { _id }',
+          'clientId { _id }',
+          'candidateId { _id }',
+          'interviewerId { _id }',
+          'userId { _id }',
+          'dateTime', 'mode', 'note', 'createdAt'
+        ]
       }))
 
       if(data.errors && data.errors.length > 0) {
@@ -38,10 +64,14 @@ export function getList(isLoading = true) {
           message: data.errors[0].message
         })
       } else {
+        const list = data.data.interviewsByOrganization
+
         dispatch({
           type: LIST_RESPONSE,
-          list: data.data.interviewsByOrganization
+          list
         })
+
+        window.localStorage.setItem(INTERVIEW_LIST_CACHE, JSON.stringify(list))
       }
     } catch(error) {
       dispatch({
@@ -60,17 +90,44 @@ export function getList(isLoading = true) {
 // Get single
 export function get(interviewId, isLoading = true) {
   return async dispatch => {
-    dispatch({
-      type: SINGLE_REQUEST,
-      isLoading
-    })
+    // Caching
+    const CACHE_KEY = `${ INTERVIEW_SINGLE_CACHE }.${ interviewId }`
+
+    try {
+      const item = JSON.parse(window.localStorage.getItem(CACHE_KEY))
+
+      if(item) {
+        dispatch({
+          type: SINGLE_RESPONSE,
+          item
+        })
+      } else {
+        dispatch({
+          type: SINGLE_REQUEST,
+          isLoading
+        })
+      }
+    } catch(e) {
+      dispatch({
+        type: SINGLE_REQUEST,
+        isLoading
+      })
+    }
 
     try {
       const { data } = await axios.post(API_URL, queryBuilder({
         type: 'query',
         operation: 'interview',
         data: { id: interviewId },
-        fields: ['_id', 'organizationId { _id }', 'clientId { _id }', 'candidateId { _id, name }', 'interviewerId { _id }', 'userId { _id }', 'dateTime', 'mode', 'note', 'createdAt']
+        fields: [
+          '_id',
+          'organizationId { _id }',
+          'clientId { _id }',
+          'candidateId { _id, name }',
+          'interviewerId { _id }',
+          'userId { _id }',
+          'dateTime', 'mode', 'note', 'createdAt'
+        ]
       }))
 
       if(data.errors && data.errors.length > 0) {
@@ -79,10 +136,14 @@ export function get(interviewId, isLoading = true) {
           message: data.errors[0].message
         })
       } else {
+        const item = data.data.interview
+
         dispatch({
           type: SINGLE_RESPONSE,
-          item: data.data.interview
+          item
         })
+
+        window.localStorage.setItem(CACHE_KEY, JSON.stringify(item))
       }
     } catch(error) {
       dispatch({
@@ -101,17 +162,44 @@ export function get(interviewId, isLoading = true) {
 // Get by Client
 export function getListByClient({ clientId }, isLoading = true) {
   return async dispatch => {
-    dispatch({
-      type: LIST_BY_CLIENT_REQUEST,
-      isLoading
-    })
+    // Caching
+    const CACHE_KEY = `${ INTERVIEW_LIST_BY_CLIENT_CACHE }.${ clientId }`
+
+    try {
+      const list = JSON.parse(window.localStorage.getItem(CACHE_KEY))
+
+      if(list) {
+        dispatch({
+          type: LIST_BY_CLIENT_RESPONSE,
+          list
+        })
+      } else {
+        dispatch({
+          type: LIST_BY_CLIENT_REQUEST,
+          isLoading
+        })
+      }
+    } catch(e) {
+      dispatch({
+        type: LIST_BY_CLIENT_REQUEST,
+        isLoading
+      })
+    }
 
     try {
       const { data } = await axios.post(API_URL, queryBuilder({
         type: 'query',
         operation: 'interviewsByClient',
         data: { clientId },
-        fields: ['_id', 'organizationId { _id, name }', 'clientId { _id, name }', 'candidateId { _id, name }', 'interviewerId { _id, name, email, mobile }', 'userId { _id, name }', 'dateTime', 'mode', 'note', 'createdAt']
+        fields: [
+          '_id',
+          'organizationId { _id, name }',
+          'clientId { _id, name }',
+          'candidateId { _id, name }',
+          'interviewerId { _id, name, email, mobile }',
+          'userId { _id, name }',
+          'dateTime', 'mode', 'note', 'createdAt'
+        ]
       }))
 
       if(data.errors && data.errors.length > 0) {
@@ -120,10 +208,14 @@ export function getListByClient({ clientId }, isLoading = true) {
           message: data.errors[0].message
         })
       } else {
+        const list = data.data.interviewsByClient
+
         dispatch({
           type: LIST_BY_CLIENT_RESPONSE,
-          list: data.data.interviewsByClient
+          list
         })
+
+        window.localStorage.setItem(CACHE_KEY, JSON.stringify(list))
       }
     } catch(error) {
       dispatch({
--- code/web/src/modules/job/api/actions/query.js
+++ code/web/src/modules/job/api/actions/query.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 import { API_URL } from '../../../../setup/config/env'
 import { queryBuilder } from '../../../../setup/helpers'
 import { MESSAGE_SHOW } from '../../../common/api/actions'
+import { JOB_LIST_CACHE, JOB_SINGLE_CACHE, JOB_LIST_BY_CLIENT_CACHE } from './cache-keys'
 import {
   LIST_REQUEST,
   LIST_RESPONSE,
@@ -20,11 +21,28 @@ import {
 // Get list
 export function getList(isLoading = true) {
   return async dispatch => {
-    dispatch({
-      type: LIST_REQUEST,
-      isLoading
-    })
+    // Caching
+    try {
+      const list = JSON.parse(window.localStorage.getItem(JOB_LIST_CACHE))
 
+      if(list) {
+        dispatch({
+          type: LIST_RESPONSE,
+          list
+        })
+      } else {
+        dispatch({
+          type: LIST_REQUEST,
+          isLoading
+        })
+      }
+    } catch(e) {
+      dispatch({
+        type: LIST_REQUEST,
+        isLoading
+      })
+    }
+    
     try {
       const { data } = await axios.post(API_URL, queryBuilder({
         type: 'query',
@@ -38,10 +56,14 @@ export function getList(isLoading = true) {
           message: data.errors[0].message
         })
       } else {
+        const list = data.data.jobsByOrganization
+
         dispatch({
           type: LIST_RESPONSE,
-          list: data.data.jobsByOrganization
+          list
         })
+
+        window.localStorage.setItem(JOB_LIST_CACHE, JSON.stringify(list))
       }
     } catch(error) {
       dispatch({
@@ -60,10 +82,29 @@ export function getList(isLoading = true) {
 // Get single
 export function get(jobId, isLoading = true) {
   return async dispatch => {
-    dispatch({
-      type: SINGLE_REQUEST,
-      isLoading
-    })
+    // Caching
+    const CACHE_KEY = `${ JOB_SINGLE_CACHE }.${ jobId }`
+
+    try {
+      const item = JSON.parse(window.localStorage.getItem(CACHE_KEY))
+
+      if(item) {
+        dispatch({
+          type: SINGLE_RESPONSE,
+          item
+        })
+      } else {
+        dispatch({
+          type: SINGLE_REQUEST,
+          isLoading
+        })
+      }
+    } catch(e) {
+      dispatch({
+        type: SINGLE_REQUEST,
+        isLoading
+      })
+    }
 
     try {
       const { data } = await axios.post(API_URL, queryBuilder({
@@ -79,10 +120,14 @@ export function get(jobId, isLoading = true) {
           message: data.errors[0].message
         })
       } else {
+        const item = data.data.job
+
         dispatch({
           type: SINGLE_RESPONSE,
-          item: data.data.job
+          item
         })
+
+        window.localStorage.setItem(CACHE_KEY, JSON.stringify(item))
       }
     } catch(error) {
       dispatch({
@@ -101,10 +146,29 @@ export function get(jobId, isLoading = true) {
 // Get by Client
 export function getListByClient({ clientId }, isLoading = true) {
   return async dispatch => {
-    dispatch({
-      type: LIST_BY_CLIENT_REQUEST,
-      isLoading
-    })
+    // Caching
+    const CACHE_KEY = `${ JOB_LIST_BY_CLIENT_CACHE }.${ clientId }`
+
+    try {
+      const list = JSON.parse(window.localStorage.getItem(CACHE_KEY))
+
+      if(list) {
+        dispatch({
+          type: LIST_BY_CLIENT_RESPONSE,
+          list
+        })
+      } else {
+        dispatch({
+          type: LIST_BY_CLIENT_REQUEST,
+          isLoading
+        })
+      }
+    } catch(e) {
+      dispatch({
+        type: LIST_BY_CLIENT_REQUEST,
+        isLoading
+      })
+    }
 
     try {
       const { data } = await axios.post(API_URL, queryBuilder({
@@ -120,10 +184,14 @@ export function getListByClient({ clientId }, isLoading = true) {
           message: data.errors[0].message
         })
       } else {
+        const list = data.data.jobsByClient
+
         dispatch({
           type: LIST_BY_CLIENT_RESPONSE,
-          list: data.data.jobsByClient
+          list
         })
+
+        window.localStorage.setItem(CACHE_KEY, JSON.stringify(list))
       }
     } catch(error) {
       dispatch({
