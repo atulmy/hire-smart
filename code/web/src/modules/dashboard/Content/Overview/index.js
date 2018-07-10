32a2b69603334867e4b39d95f677c05e8664af0f
atulmy <atul.12788@gmail.com>
2018-07-10 22:39:00 +0530

    Minor fixes

--- code/web/src/modules/dashboard/Content/Overview/index.js
+++ code/web/src/modules/dashboard/Content/Overview/index.js
@@ -65,8 +65,8 @@ class Overview extends PureComponent {
 
   columnWidth = () => {
     let width = parseInt((window.innerWidth - 250) / params.kanban.columns.length, 10) - 1
-    if(width < 225) {
-      width = 225
+    if(width < 200) {
+      width = 200
     }
     return width
   }
