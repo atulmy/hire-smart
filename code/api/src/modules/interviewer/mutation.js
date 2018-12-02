1637bfb5c51cc05241f6ce7c8cf8388f8fcd23a1
atulmy <atul.12788@gmail.com>
2018-12-02 18:27:42 +0530

    Interviewer validations

--- code/api/src/modules/interviewer/mutation.js
+++ code/api/src/modules/interviewer/mutation.js
@@ -1,79 +1,172 @@
-// Imports
-import isEmpty from 'lodash/isEmpty'
-
 // App Imports
 import params from '../../setup/config/params'
 import { authCheck } from '../../setup/helpers/utils'
+import validate from '../../setup/helpers/validation'
 import Activity from '../activity/model'
 import Interviewer from './model'
 
 // Create
 export async function interviewerCreate({ params: { projectId, name, email, mobile }, auth }) {
   if(authCheck(auth)) {
-    const interviewer = await Interviewer.create({
-      organizationId: auth.user.organizationId,
-      userId: auth.user._id,
-      projectId,
-      name,
-      email,
-      mobile
-    })
+    // Validation rules
+    const rules = [
+      {
+        data: { value: projectId },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      },
+      {
+        data: { value: name },
+        check: 'notEmpty',
+        message: 'Please enter valid name.'
+      },
+      {
+        data: { value: email },
+        check: 'email',
+        message: 'Please enter valid email.'
+      },
+      {
+        data: { value: mobile },
+        check: 'notEmpty',
+        message: 'Please enter valid mobile number.'
+      }
+    ]
 
-    if(interviewer) {
-      // Log activity
-      await Activity.create({
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const interviewer = await Interviewer.create({
         organizationId: auth.user.organizationId,
         userId: auth.user._id,
         projectId,
-        interviewerId: interviewer._id,
-        action: params.activity.types.create,
-        message: `${ auth.user.name } added a new interviewer ${ name } (${ email }).`
+        name,
+        email,
+        mobile
       })
-    }
 
-    return {
-      data: interviewer
+      if(interviewer) {
+        // Log activity
+        await Activity.create({
+          organizationId: auth.user.organizationId,
+          userId: auth.user._id,
+          projectId,
+          interviewerId: interviewer._id,
+          action: params.activity.types.create,
+          message: `${ auth.user.name } added a new interviewer ${ name } (${ email }).`
+        })
+      }
+
+      return {
+        data: interviewer
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Update
 export async function interviewerUpdate({ params: { id, projectId, name, email, mobile }, auth }) {
-  if(authCheck(auth) && !isEmpty(id)) {
-    const data = await Interviewer.updateOne(
-      { _id: id },
+  if(authCheck(auth)) {
+    // Validation rules
+    const rules = [
       {
-        $set: {
-          projectId,
-          name,
-          email,
-          mobile
-        }
+        data: { value: id },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      },
+      {
+        data: { value: projectId },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      },
+      {
+        data: { value: name },
+        check: 'notEmpty',
+        message: 'Please enter valid name.'
+      },
+      {
+        data: { value: email },
+        check: 'email',
+        message: 'Please enter valid email.'
+      },
+      {
+        data: { value: mobile },
+        check: 'notEmpty',
+        message: 'Please enter valid mobile number.'
       }
-    )
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Interviewer.updateOne(
+        { _id: id },
+        {
+          $set: {
+            projectId,
+            name,
+            email,
+            mobile
+          }
+        }
+      )
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Delete
 export async function interviewerRemove({ params: { id }, auth }) {
   if(authCheck(auth)) {
-    const data = await Interviewer.remove({
-      _id: _id,
-      userId: auth.user._id
-    })
+    // Validation rules
+    const rules = [
+      {
+        data: { value: id },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      }
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Interviewer.remove({
+        _id: id,
+        userId: auth.user._id
+      })
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
--- code/api/src/modules/interviewer/query.js
+++ code/api/src/modules/interviewer/query.js
@@ -1,64 +1,97 @@
 // App Imports
+import params from '../../setup/config/params'
 import { authCheck } from '../../setup/helpers/utils'
+import validate from '../../setup/helpers/validation'
 import Interviewer from './model'
 
 // Get interviewer by ID
 export async function interviewer({ params: { id }}) {
   if(authCheck(auth)) {
-    const data = await Interviewer.findOne({ _id: id })
+    // Validation rules
+    const rules = [
+      {
+        data: { value: id },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      }
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Interviewer.findOne({ _id: id })
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Get by project
 export async function interviewersByProject({ params: { projectId }, auth }) {
   if(authCheck(auth)) {
-    const data = await Interviewer.find({
-      organizationId: auth.user.organizationId,
-      projectId
-    })
-      .populate('organizationId')
-      .populate('projectId')
+    // Validation rules
+    const rules = [
+      {
+        data: { value: projectId },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      }
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Interviewer.find({
+        organizationId: auth.user.organizationId,
+        projectId
+      })
+        .populate('organizationId')
+        .populate('projectId')
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Get by organization
 export async function interviewersByOrganization({ auth }) {
   if(authCheck(auth)) {
-    const data = await Interviewer.find({
-      organizationId: auth.user.organizationId
-    })
-      .populate('organizationId')
-      .populate('projectId')
+    try {
+      const data = await Interviewer.find({
+        organizationId: auth.user.organizationId
+      })
+        .populate('organizationId')
+        .populate('projectId')
 
-    return {
-      data
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
-}
-
-// Get all
-export async function interviewers() {
-  if(authCheck(auth)) {
-    const data = await Interviewer.find()
-
-    return {
-      data
-    }
-  }
-
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
