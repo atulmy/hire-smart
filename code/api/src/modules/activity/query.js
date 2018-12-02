e8520b4d98662ce901e922157959d3b1a453e34a
atulmy <atul.12788@gmail.com>
2018-12-02 18:48:51 +0530

    Activity validations

--- code/api/src/modules/activity/query.js
+++ code/api/src/modules/activity/query.js
@@ -1,55 +1,117 @@
 // App Imports
+import params from '../../setup/config/params'
 import { authCheck } from '../../setup/helpers/utils'
+import validate from '../../setup/helpers/validation'
 import Activity from './model'
 
 // Get by project
 export async function activitiesByOrganization({ params: { projectId }, auth }) {
   if(authCheck(auth)) {
-    const data = await Activity.find({
-      organizationId: auth.user.organizationId
-    })
-      .sort({ createdAt: -1 })
-      .populate('userId')
+    // Validation rules
+    const rules = [
+      {
+        data: { value: projectId },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      }
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Activity.find({
+        organizationId: auth.user.organizationId
+      })
+        .sort({ createdAt: -1 })
+        .populate('userId')
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error.server)
     }
   }
 
-  throw new Error('Please login to view activities.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Get by project
 export async function activitiesByProject({ params: { projectId }, auth }) {
   if(authCheck(auth)) {
-    const data = await Activity.find({
-      organizationId: auth.user.organizationId,
-      projectId
-    })
-      .sort({ createdAt: -1 })
-      .populate('userId')
+    // Validation rules
+    const rules = [
+      {
+        data: { value: projectId },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      }
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Activity.find({
+        organizationId: auth.user.organizationId,
+        projectId
+      })
+        .sort({ createdAt: -1 })
+        .populate('userId')
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error.server)
     }
   }
 
-  throw new Error('Please login to view activities.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Get by candidate
 export async function activitiesByCandidate({ params: { candidateId }, auth }) {
   if(authCheck(auth)) {
-    const data = await Activity.find({
-      organizationId: auth.user.organizationId,
-      candidateId
-    })
-      .sort({ createdAt: 1 })
+    // Validation rules
+    const rules = [
+      {
+        data: { value: candidateId },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      }
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Activity.find({
+        organizationId: auth.user.organizationId,
+        candidateId
+      })
+        .sort({ createdAt: 1 })
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error.server)
     }
   }
 
-  throw new Error('Please login to view activities.')
+  throw new Error(params.user.message.error.auth)
 }
