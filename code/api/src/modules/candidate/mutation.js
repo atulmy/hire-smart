3e34ef49167f98112b141412e8103bdae424a929
atulmy <atul.12788@gmail.com>
2018-12-02 18:47:18 +0530

    Candidate validations

--- code/api/src/modules/candidate/mutation.js
+++ code/api/src/modules/candidate/mutation.js
@@ -4,6 +4,7 @@ import isEmpty from 'lodash/isEmpty'
 // App Imports
 import params from '../../setup/config/params'
 import { authCheck } from '../../setup/helpers/utils'
+import validate from '../../setup/helpers/validation'
 import Kanban from '../kanban/model'
 import Activity from '../activity/model'
 import Candidate from './model'
@@ -11,103 +12,218 @@ import Candidate from './model'
 // Create
 export async function candidateCreate({ params: { projectId, jobId = '', name, email, mobile, experience, resume, salaryCurrent = '', salaryExpected = '' }, auth }) {
   if(authCheck(auth)) {
-    let item = {
-      organizationId: auth.user.organizationId,
-      projectId,
-      userId: auth.user._id,
-      name,
-      email,
-      mobile,
-      experience,
-      resume,
-      salaryCurrent,
-      salaryExpected
-    }
-    if(!isEmpty(jobId)) {
-      item.jobId = jobId
+    // Validation rules
+    const rules = [
+      {
+        data: { value: projectId },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      },
+      {
+        data: { value: name },
+        check: 'notEmpty',
+        message: 'Please enter a valid name.'
+      },
+      {
+        data: { value: email },
+        check: 'email',
+        message: 'Please enter a valid email.'
+      },
+      {
+        data: { value: mobile },
+        check: 'mobile',
+        message: 'Please enter a valid mobile number.'
+      },
+      {
+        data: { value: experience },
+        check: 'notEmpty',
+        message: 'Please enter a valid experience in years.'
+      },
+      {
+        data: { value: resume },
+        check: 'notEmpty',
+        message: 'Please upload resume.'
+      }
+    ]
+
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
     }
 
-    const candidate = await Candidate.create(item)
-
-    if(candidate) {
-      const kanban = await Kanban.create({
+    try {
+      let item = {
         organizationId: auth.user.organizationId,
         projectId,
-        candidateId: candidate._id,
         userId: auth.user._id,
-        status: params.kanban.columns[0].key,
-        highlight: false
-      })
+        name,
+        email,
+        mobile,
+        experience,
+        resume,
+        salaryCurrent,
+        salaryExpected
+      }
+      if(!isEmpty(jobId)) {
+        item.jobId = jobId
+      }
 
-      // Log activity
-      await Activity.create({
-        organizationId: auth.user.organizationId,
-        userId: auth.user._id,
-        projectId,
-        candidateId: candidate._id,
-        kanbanId: kanban._id,
-        action: params.activity.types.create,
-        message: `${ auth.user.name } added a new candidate ${ name } (${ email }).`
-      })
-    }
+      const candidate = await Candidate.create(item)
 
-    return {
-      data: candidate
+      if(candidate) {
+        const kanban = await Kanban.create({
+          organizationId: auth.user.organizationId,
+          projectId,
+          candidateId: candidate._id,
+          userId: auth.user._id,
+          status: params.kanban.columns[0].key,
+          highlight: false
+        })
+
+        // Log activity
+        await Activity.create({
+          organizationId: auth.user.organizationId,
+          userId: auth.user._id,
+          projectId,
+          candidateId: candidate._id,
+          kanbanId: kanban._id,
+          action: params.activity.types.create,
+          message: `${ auth.user.name } added a new candidate ${ name } (${ email }).`
+        })
+      }
+
+      return {
+        data: candidate
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error.server)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Update
 export async function candidateUpdate({ params: { id, projectId, jobId = '', name, email, mobile, experience, resume, salaryCurrent = '', salaryExpected = '' }, auth }) {
   if(authCheck(auth) && !isEmpty(id)) {
-    let item = {
-      projectId,
-      name,
-      email,
-      mobile,
-      experience,
-      resume,
-      salaryCurrent,
-      salaryExpected
-    }
-    if(!isEmpty(jobId)) {
-      item.jobId = jobId
+    // Validation rules
+    const rules = [
+      {
+        data: { value: id },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      },
+      {
+        data: { value: projectId },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      },
+      {
+        data: { value: name },
+        check: 'notEmpty',
+        message: 'Please enter a valid name.'
+      },
+      {
+        data: { value: email },
+        check: 'email',
+        message: 'Please enter a valid email.'
+      },
+      {
+        data: { value: mobile },
+        check: 'mobile',
+        message: 'Please enter a valid mobile number.'
+      },
+      {
+        data: { value: experience },
+        check: 'notEmpty',
+        message: 'Please enter a valid experience in years.'
+      },
+      {
+        data: { value: resume },
+        check: 'notEmpty',
+        message: 'Please upload resume.'
+      }
+    ]
+
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
     }
 
-    const candidate = await Candidate.updateOne({ _id: id }, { $set: item })
+    try {
+      let item = {
+        projectId,
+        name,
+        email,
+        mobile,
+        experience,
+        resume,
+        salaryCurrent,
+        salaryExpected
+      }
+      if(!isEmpty(jobId)) {
+        item.jobId = jobId
+      }
 
-    if(candidate) {
-      await Kanban.updateOne(
-        {
-          organizationId: auth.user.organizationId,
-          candidateId: id
-        },
-        { projectId }
-      )
-    }
+      const candidate = await Candidate.updateOne({ _id: id }, { $set: item })
 
-    return {
-      data: candidate
+      if(candidate) {
+        await Kanban.updateOne(
+          {
+            organizationId: auth.user.organizationId,
+            candidateId: id
+          },
+          { projectId }
+        )
+      }
+
+      return {
+        data: candidate
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error.server)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Delete
 export async function candidateRemove({ params: { id }, auth }) {
   if(authCheck(auth)) {
-    const data = await Candidate.remove({
-      _id: _id,
-      userId: auth.user._id
-    })
+    // Validation rules
+    const rules = [
+      {
+        data: { value: id },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      }
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Candidate.remove({
+        _id: _id,
+        userId: auth.user._id
+      })
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error.server)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
--- code/api/src/modules/candidate/query.js
+++ code/api/src/modules/candidate/query.js
@@ -1,59 +1,96 @@
 // App Imports
+import params from '../../setup/config/params'
 import { authCheck } from '../../setup/helpers/utils'
+import validate from '../../setup/helpers/validation'
 import Candidate from './model'
 
 // Get candidate by ID
 export async function candidate({ params: { id }}) {
-  const data = await Candidate.findOne({ _id: id })
-    .populate('projectId')
-    .populate('jobId')
+  // Validation rules
+  const rules = [
+    {
+      data: { value: id },
+      check: 'notEmpty',
+      message: params.common.message.error.invalidData
+    }
+  ]
 
-  return {
-    data
+  // Validate
+  try {
+    validate(rules)
+  } catch(error) {
+    throw new Error(error.message)
+  }
+
+  try {
+    const data = await Candidate.findOne({ _id: id })
+      .populate('projectId')
+      .populate('jobId')
+
+    return {
+      data
+    }
+  } catch(error) {
+    throw new Error(params.common.message.error.server)
   }
 }
 
 // Get by project
 export async function candidatesByProject({ params: { projectId }, auth }) {
   if(authCheck(auth)) {
-    const data = await Candidate.find({
-      organizationId: auth.user.organizationId,
-      projectId
-    })
-      .populate('projectId')
-      .populate('jobId')
+    // Validation rules
+    const rules = [
+      {
+        data: { value: projectId },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      }
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Candidate.find({
+        organizationId: auth.user.organizationId,
+        projectId
+      })
+        .populate('projectId')
+        .populate('jobId')
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error.server)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Get by organization
 export async function candidatesByOrganization({ auth }) {
   if(authCheck(auth)) {
-    const data = await Candidate.find({
-      organizationId: auth.user.organizationId
-    })
-      .populate('organizationId')
-      .populate('projectId')
-      .populate('jobId')
+    try {
+      const data = await Candidate.find({
+        organizationId: auth.user.organizationId
+      })
+        .populate('organizationId')
+        .populate('projectId')
+        .populate('jobId')
 
-    return {
-      data
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error.server)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
-}
-
-// Get all
-export async function candidates() {
-  const data = await Candidate.find()
-
-  return {
-    data
-  }
+  throw new Error(params.user.message.error.auth)
 }
