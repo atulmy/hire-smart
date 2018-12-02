9ea81d3ae2f3fbcdb4c4b591649db36b4ce5fb83
atulmy <atul.12788@gmail.com>
2018-12-02 17:21:48 +0530

    Job validations

--- code/api/src/modules/job/mutation.js
+++ code/api/src/modules/job/mutation.js
@@ -1,77 +1,145 @@
-// Imports
-import isEmpty from 'lodash/isEmpty'
-
 // App Imports
 import params from '../../setup/config/params'
 import { authCheck } from '../../setup/helpers/utils'
+import validate from '../../setup/helpers/validation'
 import Activity from '../activity/model'
 import Job from './model'
 
 // Create
 export async function jobCreate({ params: { projectId, role, description = '' }, auth }) {
   if(authCheck(auth)) {
-    const job = await Job.create({
-      organizationId: auth.user.organizationId,
-      projectId,
-      userId: auth.user._id,
-      role,
-      description
-    })
+    // Validation rules
+    const rules = [
+      {
+        data: { value: projectId },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      },
+      {
+        data: { value: role },
+        check: 'notEmpty',
+        message: 'Please enter a valid role.'
+      }
+    ]
 
-    if(job) {
-      // Log activity
-      await Activity.create({
-        organizationId: auth.user.organizationId,
-        userId: auth.user._id,
-        projectId,
-        jobId: job._id,
-        action: params.activity.types.create,
-        message: `${ auth.user.name } added a new job ${ role }.`
-      })
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
     }
 
-    return {
-      data: job
+    try {
+      const job = await Job.create({
+        organizationId: auth.user.organizationId,
+        projectId,
+        userId: auth.user._id,
+        role,
+        description
+      })
+
+      if(job) {
+        // Log activity
+        await Activity.create({
+          organizationId: auth.user.organizationId,
+          userId: auth.user._id,
+          projectId,
+          jobId: job._id,
+          action: params.activity.types.create,
+          message: `${ auth.user.name } added a new job ${ role }.`
+        })
+      }
+
+      return {
+        data: job
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Update
 export async function jobUpdate({ params: { id, projectId, role, description = '' }, auth }) {
-  if(authCheck(auth) && !isEmpty(id)) {
-    const data = await Job.updateOne(
-      { _id: id },
+  if(authCheck(auth)) {
+    // Validation rules
+    const rules = [
       {
-        $set: {
-          projectId,
-          role,
-          description
-        }
+        data: { value: id },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      },
+      {
+        data: { value: role },
+        check: 'notEmpty',
+        message: 'Please enter a valid role.'
       }
-    )
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Job.updateOne(
+        { _id: id },
+        {
+          $set: {
+            projectId,
+            role,
+            description
+          }
+        }
+      )
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Delete
 export async function jobRemove(parentValue, { id }, { auth }) {
   if(authCheck(auth)) {
-    const data = await Job.remove({
-      _id: _id,
-      userId: auth.user._id
-    })
+    // Validation rules
+    const rules = [
+      {
+        data: { value: id },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      }
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Job.remove({
+        _id: _id,
+        userId: auth.user._id
+      })
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
--- code/api/src/modules/job/query.js
+++ code/api/src/modules/job/query.js
@@ -1,45 +1,74 @@
 // App Imports
+import params from '../../setup/config/params'
 import { authCheck } from '../../setup/helpers/utils'
+import validate from '../../setup/helpers/validation'
 import Job from './model'
 
 // Get job by ID
 export async function job({ params: { id } }) {
   if(authCheck(auth)) {
-    const data = await Job.findOne({ _id: id })
+    // Validation rules
+    const rules = [
+      {
+        data: { value: id },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      }
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Job.findOne({ _id: id })
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
 
 // Get by project
 export async function jobsByProject({ params: { projectId }, auth }) {
   if(authCheck(auth)) {
-    const data = await Job.find({
-      organizationId: auth.user.organizationId,
-      projectId
-    })
+    // Validation rules
+    const rules = [
+      {
+        data: { value: projectId },
+        check: 'notEmpty',
+        message: params.common.message.error.invalidData
+      }
+    ]
 
-    return {
-      data
+    // Validate
+    try {
+      validate(rules)
+    } catch(error) {
+      throw new Error(error.message)
+    }
+
+    try {
+      const data = await Job.find({
+        organizationId: auth.user.organizationId,
+        projectId
+      })
+
+      return {
+        data
+      }
+    } catch(error) {
+      throw new Error(params.common.message.error)
     }
   }
 
-  throw new Error('You are not allowed to perform this action.')
-}
-
-// Get all
-export async function jobs() {
-  if(authCheck(auth)) {
-    const data = await Job.find()
-
-    return {
-      data
-    }
-  }
-
-  throw new Error('You are not allowed to perform this action.')
+  throw new Error(params.user.message.error.auth)
 }
