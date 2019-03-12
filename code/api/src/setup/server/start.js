201df89f74c1c4bf4ac5588392bc70739aae4b99
atulmy <atul.12788@gmail.com>
2019-03-12 22:57:56 +0530

    Database connectivity fix

--- code/api/src/setup/server/start.js
+++ code/api/src/setup/server/start.js
@@ -1,5 +1,6 @@
 // Imports
 import ip from 'ip'
+import mongoose from 'mongoose'
 
 // App Imports
 import { PORT, NODE_ENV } from '../config/env'
@@ -8,19 +9,31 @@ import { PORT, NODE_ENV } from '../config/env'
 export default function (server) {
   console.info('SETUP - Starting server..')
 
-  server.listen(PORT, (error) => {
+  const serverProcess = server.listen(PORT, (error) => {
     if (error) {
       console.error('ERROR - Unable to start server.')
     } else {
       console.info(`INFO - Server started on`)
       console.info(`  Local   http://localhost:${ PORT } [${ NODE_ENV }]`)
-      console.info(`  Network http://${ ip.address() }:${ PORT } [${ NODE_ENV }] \n`)
+      console.info(`  Network http://${ ip.address() }:${ PORT } [${ NODE_ENV }]`)
+      console.info(`  Datetime ${ new Date() }\n`)
     }
   })
 
-  process.on('SIGTERM', function () {
-    server.close(function () {
-      console.log('Finished all requests. Server stopped.')
+  // Stop Server
+  for(let signal of ['SIGINT', 'SIGTERM']) {
+    process.on(signal, function () {
+      console.info('INFO - Shutting down server..')
+
+      serverProcess.close(function () {
+        console.info('INFO - Server has been shut down.')
+
+        mongoose.connection.close(false, () => {
+          console.info('INFO - Database disconnected.')
+
+          process.exit(0)
+        })
+      })
     })
-  })
+  }
 }
